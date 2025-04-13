82cdd4fee4054b2166f37cd0e6bfd7d0d4b72335
zhdanol <olegzdanov87@gmail.com>
2025-04-13 13:42:38 +0700

    result

--- main.py
+++
@@ -1,8 +0,0 @@
-from application.db.people import get_people
-from salary import calculate_salary
-from datetime import datetime
-
-if __name__ == '__main__':
-    print(datetime.now())
-    get_people()
-    calculate_salary()
